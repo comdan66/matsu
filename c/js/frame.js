6d0d3e7a4850fb24c85771ccdb13806c92cbff6a
OA Wu <comdan66@gmail.com>
2015-03-23 11:11:10 +0800

    Js struce.

--- c/js/frame.js
+++ c/js/frame.js
@@ -4,10 +4,78 @@
  */
 
 $(function () {
-  $rightSlide = $('#right_slide');
-  $slideCover = $('#slide_cover');
-  $option = $('#option');
+  var $rightSlide = $('#right_slide');
+  var $slideCover = $('#slide_cover');
+  var $headerLeft = $('#header .l');
+  var $subItems = $('#sub_items');
+  var $items = $rightSlide.children ('.items');
+  var $option = $('#option');
+
   var overflow = $('body').css ('overflow');
+  var now = document.URL.replace(/^.*[\\\/]/, '');
+  var timer = null;
+
+  var menu = [
+    {'titles': ['北港媽祖'], 'sub': [
+      {'name': 'chao-tian.html', 'titles': ['朝天宮']},
+      {'name': '03-19.html', 'titles': ['三月十九']},
+      {'name': 'position.html', 'titles': ['交通位置']},
+      {'name': 'food.html', 'titles': ['小吃美食']}
+    ]},
+
+    {'titles': ['百年藝陣'], 'sub': [
+      {'name': 'din-tao.html', 'titles': ['藝陣', '文化']},
+      {'name': 'map-19an_din-tao.html', 'titles': ['十九下午', '繞境地圖']},
+      {'name': 'map-19ni_din-tao.html', 'titles': ['十九晚間', '繞境地圖']},
+      {'name': 'map-20an_din-tao.html', 'titles': ['二十下午', '繞境地圖']},
+      {'name': 'map-20ni_din-tao.html', 'titles': ['二十晚間', '繞境地圖']}
+    ]},
+
+    {'titles': ['藝閣遊行'], 'sub': [
+      {'name': 'i-ko.html', 'titles': ['藝閣文化']},
+      {'name': 'map-19an_i-ko.html', 'titles': ['十九下午', '繞境地圖']},
+      {'name': 'map-19ni_i-ko.html', 'titles': ['十九晚間', '繞境地圖']},
+      {'name': 'map-20an_i-ko.html', 'titles': ['二十下午', '繞境地圖']},
+      {'name': 'map-20ni_i-ko.html', 'titles': ['二十晚間', '繞境地圖']},
+      {'name': 'map-21ni_i-ko.html', 'titles': ['廿一晚間', '繞境地圖']},
+      {'name': 'map-22ni_i-ko.html', 'titles': ['廿二晚間', '繞境地圖']},
+      {'name': 'map-23ni_i-ko.html', 'titles': ['廿三晚間', '繞境地圖']}
+    ]},
+
+    {'titles': ['網站聲明'], 'sub': [
+      {'name': 'resource.html', 'titles': ['資料引用']},
+      {'name': 'disclaimer.html', 'titles': ['分享聲明']},
+      {'name': 'developer.html', 'titles': ['開發人員']}
+    ]}
+  ];
+
+  menu = menu.map (function (t) {
+    var has = false;
+
+    if (t.sub && t.sub.length) {
+      t.name = t.sub[0].name;
+      t.sub = t.sub.map (function (u) {
+        has |= u.active = (now == u.name);
+        return u;
+      });
+    }
+
+    t.active = has ? true : false;
+    return t;
+  });
+
+  menu.forEach (function (t) {
+    $items.append ($('<a />').addClass ('item').addClass (t.active ? 'active' : null).attr ('href', t.name).text (t.titles.join (' ')));
+    $headerLeft.append ($('<a />').addClass (t.active ? 'active' : null).attr ('href', t.name).text (t.titles.join (' ')));
+
+    if (t.sub && t.sub.length) {
+      t.sub.forEach (function (u) {$items.append ($('<a />').addClass ('sub').addClass (u.active ? 'active' : null).attr ('href', u.name).text (u.titles.join (' ')));});
+
+      if (t.active)
+        t.sub.forEach (function (u) {
+          $subItems.append ($('<a />').addClass (u.active ? 'active' : null).attr ('href', u.name).append (u.titles.map (function (v) {return $('<div />').text (v);})));});
+    }
+  });
 
   $option.click (function () {
     if ($rightSlide.hasClass ('close')) {
@@ -29,6 +97,10 @@ $(function () {
     }
   });
 
-  $('#sub_items .item').OAripple ().OAjelly ();
+  $('#sub_items a').OAripple ().OAjelly ().click (function (e) {
+    clearTimeout (timer);
+    timer = setTimeout (function () {window.location.assign ($(this).attr ('href'));}, 500);
+    return false;
+  });
 
 });
