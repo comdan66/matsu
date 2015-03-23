10dca7f53001ae9c8a484bbcca216337af7b2d95
OA Wu <comdan66@gmail.com>
2015-03-23 22:48:32 +0800

    chao-tian page temp.

--- c/js/chao-tian.js
+++ c/js/chao-tian.js
@@ -5,50 +5,31 @@
 
 $(function () {
   var $imgs = $('.imgs');
-  var time = 2000;
+  var time = 4000;
 
   $imgs.map (function () {
     var $that = $(this);
     var timer = null;
 
-    $(this).attr ('data-title', $(this).find ('img').eq (0).attr ('title')).imgLiquid ();
+    $that.addClass ('show');
+    $that.attr ('data-title', $(this).find ('img').eq (0).attr ('title')).imgLiquid ();
 
     var change = function () {
       clearTimeout (timer);
+      $that.removeClass ('show');
 
-      var $img = $that.find ('img').eq (0);
-      $that.append ($img.clone ());
-      $img.remove ();
-      $that.attr ('data-title', $that.find ('img').eq (0).attr ('title')).imgLiquid ();
+      setTimeout (function () {
+        var $img = $that.find ('img').eq (0);
+        $that.append ($img.clone ());
+        $img.remove ();
+        $that.attr ('data-title', $that.find ('img').eq (0).attr ('title')).imgLiquid ();
+        $that.addClass ('show');
+      }, 200);
 
-
-      timer = setTimeout (change, time);
+      timer = setTimeout (change, time + Math.floor (Math.random () * (2000)));
     };
 
-    timer = setTimeout (change, time);
+    timer = setTimeout (change, time + Math.floor (Math.random () * (2000)));
   });
-
-  // var $unitRight = $('.unit .r');
-
-  // var srcs = $unitRight.children ('input').map (function () {
-  //   $unitRight.append ($('<img />').attr ('src', $(this).val ())).imgLiquid ();
-  //   return $(this).val ();
-  // });
-
-
-  // var timer = null;
-  // //, index = 0;
-
-  // var change = function () {
-  //   clearTimeout (timer);
-
-  //   var $img = $unitRight.find ('img').eq (0);
-  //   $unitRight.append ($img.clone ());
-  //   $img.remove ();
-  //   $unitRight.imgLiquid ({verticalAlign: 'top'});
-
-  //   timer = setTimeout (change, 2000);
-  // };
-  // change ();
 
 });
