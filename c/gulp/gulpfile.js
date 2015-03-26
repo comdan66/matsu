3ce64ac3d79aeb1957ba41a5ba75d28f911099ef
OA Wu <comdan66@gmail.com>
2015-03-26 17:59:36 +0800

    Fix  script link.

--- c/gulp/gulpfile.js
+++ c/gulp/gulpfile.js
@@ -1,7 +1,8 @@
 var gulp = require ('gulp'),
     livereload = require('gulp-livereload'),
     uglifyJS = require ('gulp-uglify'),
-    del = require('del');
+    del = require('del'),
+    htmlmin = require('gulp-html-minifier');
 
 gulp.task ('default', function () {
   livereload.listen ();
@@ -33,10 +34,9 @@ gulp.task ('js-uglify', function () {
       .pipe (gulp.dest ('./root/js/'));
 });
 gulp.task ('minify-html', function () {
-  // gulp.src ('./root/js/*.js')
-  //     .pipe (uglifyJS ())
-  //     .pipe (gulp.dest ('./root/js/'));
-
+  gulp.src ('./root/*.html')
+    .pipe (htmlmin ({collapseWhitespace: true}))
+    .pipe (gulp.dest ('./root/'));
 });
 gulp.task ('res-uglify', function () {
   gulp.src ('./root/res/**/*.js')
