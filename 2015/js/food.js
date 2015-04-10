57487143b36f3cff5ddd53249f15348c512b2f4e
OA Wu <comdan66@gmail.com>
2015-04-10 22:37:46 +0800

    Add content temp.

--- 2015/js/food.js
+++ 2015/js/food.js
@@ -25,39 +25,17 @@ $(function () {
     {
       t: '美食文章',
       vs: [
-        {p: true, t: '[食記] 北港 小籠湯包', h: 'https://www.ptt.cc/bbs/Yunlin/M.1296565841.A.4BC.html'},
-        {p: true, t: '[食記] 雲林北港 福安鴨肉飯', h: 'http://douglas82328.pixnet.net/blog/post/109564667'},
-        {p: true, t: '[食記] 雲林北港。老受鴨肉飯', h: 'http://windko0813.pixnet.net/blog/post/107795083'},
-        {p: true, t: '[食記] 雲林北港 北港圓仔湯', h: 'http://sam76227.pixnet.net/blog/post/106376858'},
-        {p: true, t: '[食記] 北港小吃一條龍~', h: 'http://anise.pixnet.net/blog/post/29995793'},
-        {p: false, t: '[食記] 北港的阿等土豆油飯(便宜好吃)', h: 'http://alisa0415.pixnet.net/blog/post/42004303'},
-        {p: false, t: '[食記] 北港橋頭香菇肉羹', h: 'http://alisa0415.pixnet.net/blog/post/42004306'},
-        {p: true, t: '[食記] 北港的阿不倒(+2015北港燈會)', h: 'http://alisa0415.pixnet.net/blog/post/42097768'},
-        {p: false, t: '[食記] 北港武德宮之樂咖啡-財神爺廟裡喝咖啡', h: 'http://alisa0415.pixnet.net/blog/post/42026104'},
-        {p: true, t: '[食記] 北港的一郎', h: 'http://alisa0415.pixnet.net/blog/post/40724839'},
-        {p: false, t: '[食記] 雲林北港．九久醇義麵坊', h: 'http://blog.yam.com/larle/article/87603574'},
-        {p: false, t: '[食記] 雲林北港．秋月生炒鱔魚', h: 'http://blog.yam.com/larle/article/85381339'},
-        {p: true, t: '[食記] 北港鎮 煎盤粿(美味銅板美食)', h: 'http://babbitwang.pixnet.net/blog/post/108226570'},
-        {p: true, t: '[食記] 北港鎮 廟邊假魚肚(30元,超特別美食)', h: 'http://babbitwang.pixnet.net/blog/post/108226990'},
-        {p: false, t: '[食記] 北港 阿豐麵線糊油飯', h: 'http://woxko.pixnet.net/blog/post/40952866'},
-        {p: true, t: '[食記] 北港圓環邊紅燒青蛙', h: 'http://thudadai.pixnet.net/blog/post/52867036'},
-        {p: true, t: '[食記] 北港圓環邊的香菇魯肉飯', h: 'https://www.ptt.cc/bbs/Yunlin/M.1236278986.A.440.html'}
+        {p: true, t: '[食記] 北港 小籠湯包', h: 'https://www.ptt.cc/bbs/Yunlin/M.1296565841.A.4BC.html', d: '說到心得 說真的這家的話 我個人會推薦 1.湯包 2.酸辣湯!! 但是 我每次去吃都是吃乾麵啦XD 在這裡 推薦給 住北港或者要來北港玩的各位做個參考XDDD', s: 'img/site/food/02-01.jpg', l: new google.maps.LatLng (23.57351612581438, 120.29720105230808)},
+        {p: true, t: '[食記] 雲林北港 福安鴨肉飯', h: 'http://douglas82328.pixnet.net/blog/post/109564667', d: '上次一月底去北港吃過老受鴨肉飯時，就一直很想再去嘗嘗北港另外家也是人氣超高的鴨肉飯-【福安鴨肉飯】趁著禮拜四這天沒課，自己一個人機車一騎，咻~~~一下就到北港了', s: 'img/site/food/02-02.jpg', l: new google.maps.LatLng (23.565343626868415, 120.30415400862694)},
+        {p: true, t: '[食記] 雲林北港。老受鴨肉飯', h: 'http://windko0813.pixnet.net/blog/post/107795083', d: '北港有名的食物，除了大餅、花生、麻油之外，應該就是鴨肉飯與鴨肉粳了吧！如果沒點名到的北港食物，就sorry啦，因為windko也太久沒去北港晃晃了。小時候其實每年都會來北港幾趟，小時候的北港燈會超有名的！還要來朝天宮拜拜！', s: 'img/site/food/02-03.jpg', l: new google.maps.LatLng (23.566428446740897, 120.30440211296082)},
+        {p: true, t: '[食記] 雲林北港 北港圓仔湯', h: 'http://sam76227.pixnet.net/blog/post/106376858', d: '還在北港讀書的時候~最常吃的剉冰就是三代豆花另一間就是北港圓仔湯~圓仔湯有冰的和熱的~我是都吃冰的啦~如果女生月事來~這裡也有熱的紅豆湯~聽說還不錯~最近回到北港~吃北港圓仔湯的次數還比較高~因為這麼多年才漲五元~算是很有良心的店家~', s: 'img/site/food/02-04.jpg', l: new google.maps.LatLng (23.565172759238962, 120.30411645770073)},
+        {p: false, t: '[食記] 北港的阿等土豆油飯(便宜好吃)', h: 'http://alisa0415.pixnet.net/blog/post/42004303', d: '↑↑↑猜猜這些總共多少錢???.土豆油飯20+爌肉飯25+蚵仔酥35+特製香腸20+燙青菜20+蘆筍沙拉35=155元.真的便宜到會嚇死人！！！', s: 'img/site/food/02-05.jpg', l: new google.maps.LatLng (23.57071047826772, 120.30380666255951)},
+        {p: false, t: '[食記] 北港橋頭香菇肉羹', h: 'http://alisa0415.pixnet.net/blog/post/42004306', d: '這間北港陸橋旁的肉羹麵很神奇.每次經過時幾乎都客滿.有一次還看到門口一長排的排隊人潮.而且都是本地人.我常吃對面的阿婆煎盤粿.一直想找機會吃這家看看.後來吃過兩次感覺都不錯.', s: 'img/site/food/02-06.jpg', l: new google.maps.LatLng (23.56649236785899, 120.30239783227444)},
+        {p: true, t: '[食記] 北港的阿不倒(+2015北港燈會)', h: 'http://alisa0415.pixnet.net/blog/post/42097768', d: '網路上蒐尋"阿不倒"，看到好幾個誇張的關鍵字，例如［好吃到讓人流淚］、［沒吃到會讓人流淚］，所以說有吃到和沒吃到都會流淚就是了，大家形容的太over了啦！', s: 'img/site/food/02-07.jpg', l: new google.maps.LatLng (23.566267414555526, 120.30375234782696)},
+        {p: false, t: '[食記] 北港武德宮之樂咖啡-財神爺廟裡喝咖啡', h: 'http://alisa0415.pixnet.net/blog/post/42026104', d: '早就聽說過北港財神爺廟裡有一家咖啡店.聽起來就很潮很有趣樣.但一直沒機會去.今天大年初四和某個朋友約好要碰面聊天一下.喜歡文青文創的她提議這家咖啡店.', s:'img/site/food/02-08.jpg', l: new google.maps.LatLng (23.581215522612414, 120.29864743351936)},
+        {p: true, t: '[食記] 北港的一郎', h: 'http://alisa0415.pixnet.net/blog/post/40724839', d: '這間一郎也是我家挑嘴的小弟喜歡的店家之一.如果晚上經過北港.常要幫他外帶豬腳和蝦仁飯回去.之前有介紹過兩次了.這次是發現它換新店址了.拍給大家看一下~~', s: 'img/site/food/02-09.jpg', l: new google.maps.LatLng (23.568582694203908, 120.30486680567265)},
       ]
     },
-    {
-      t: '住宿文章',
-      vs: [
-        {p: false, t: '[心得] 雲林北港 笨港客棧(1200元/雙人房)', h: 'http://babbitwang.pixnet.net/blog/post/108226279'},
-        {p: false, t: '[雲記] 北港 金山商務旅館 住宿心得', h: 'http://thudadai.pixnet.net/blog/post/59263852'}
-      ]
-    },
-    {
-      t: '其他文章',
-      vs: [
-        {p: false, t: '[問題] 北港名產買哪家', h: 'https://www.ptt.cc/bbs/Yunlin/M.1426173181.A.775.html'},
-        {p: false, t: '[食記] 北港鴨肉飯 (老受.福安比較)', h: 'http://yaoching2003043.pixnet.net/blog/post/29569910'}
-      ]
-    }
   ];
     //   {
     //     t: '旅遊文章',
