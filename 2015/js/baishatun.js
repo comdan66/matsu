922c44291371e7751c186ae19c87f228ff15534b
OA Wu <comdan66@gmail.com>
2015-05-22 23:35:59 +0800

    Fix bug.

--- 2015/js/baishatun.js
+++ 2015/js/baishatun.js
@@ -10,7 +10,7 @@ $(function () {
 
   var map = null;
   var latlngs = [];
-  var marks = [];
+  var markers = [];
   var polyline = null;
   var $loading = null;
 
@@ -91,8 +91,8 @@ $(function () {
               center: new google.maps.LatLng (latlngs[latlngs.length - 1].lat, latlngs[latlngs.length - 1].lng)
             });
 
-        if (marks.length)
-          marks[marks.length - 1].setIcon ({
+        if (markers.length)
+          markers[markers.length - 1].setIcon ({
             path: circlePath (10),
             strokeColor: 'rgba(249, 39, 114, 1)',
             strokeWeight: 1,
@@ -100,8 +100,8 @@ $(function () {
             fillOpacity: 0.5
           });
 
-        marks = marks.concat (latlngs.map (function (t, i) {
-                  var x = new google.maps.Marker ({
+        markers = markers.concat (latlngs.map (function (t, i) {
+                  var marker = new google.maps.Marker ({
                       map: map,
                       draggable: false,
                       position: new google.maps.LatLng (t.lat, t.lng),
@@ -114,10 +114,10 @@ $(function () {
                       }
                     });
 
-                  google.maps.event.addListener (x, 'click', function (e) {
+                  google.maps.event.addListener (marker, 'click', function (e) {
                     console.error (t.id);
                   });
-                  return x;
+                  return marker;
                 }));
 
         if (!polyline)
@@ -126,7 +126,7 @@ $(function () {
             strokeColor: 'rgba(249, 39, 114, .15)',
             strokeWeight: 10
           });
-        polyline.setPath (marks.map (function (t) { return t.position; }));
+        polyline.setPath (markers.map (function (t) { return t.position; }));
 
         if (!$loading)
           $loading = $('#loading').fadeOut (function () {
@@ -137,7 +137,7 @@ $(function () {
         else
           mapGo (new google.maps.LatLng (latlngs[latlngs.length - 1].lat, latlngs[latlngs.length - 1].lng));
 
-        setTimeout (calculateLength.bind (this, marks.map (function (t) { return t.position; })), 1800);
+        setTimeout (calculateLength.bind (this, markers.map (function (t) { return t.position; })), 1800);
 
       });
     };
